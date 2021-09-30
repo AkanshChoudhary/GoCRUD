a197ee1a6378a41dafad26c3487b37761ff27081
akanshchoudhary79@gmail.com <Akansh1974>
2021-09-30 11:36:21 +0530

    second commit

--- main_test.go
+++ main_test.go
@@ -17,3 +17,13 @@ func TestDeleteUser(t *testing.T) {
 	r := gin.Default()
 	r.DELETE("/delete/John", deleteUser)
 }
+
+func TestAddUser(t *testing.T) {
+	r := gin.Default()
+	r.POST("/create", createUser)
+}
+
+func TestUpdateUser(t *testing.T) {
+	r := gin.Default()
+	r.POST("/update", updateUser)
+}
